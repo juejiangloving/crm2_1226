6ac0dd418ee4dc53d7711a885d249dbd0cd95e9a
hxf <hxf199248@163.com>
2016-05-27 21:16:28 +0800

    同步

--- src/com/atguigu/crm/test/TestGit.java
+++ src/com/atguigu/crm/test/TestGit.java
@@ -4,6 +4,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("1111111");
 		System.out.println("2222222");
+		System.out.println("3333333");
+		System.out.println("4444444");
 	}
 
 }
