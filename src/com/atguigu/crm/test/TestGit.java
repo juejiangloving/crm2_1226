de72713db7d67adbc35a44fa401f04f84842f504
juejiangloving <hxf199248@163.com>
2016-05-27 21:14:50 +0800

    u

--- src/com/atguigu/crm/test/TestGit.java
+++ src/com/atguigu/crm/test/TestGit.java
@@ -4,6 +4,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("1111111");
 		System.out.println("2222222");
+		System.out.println("5555555");
+		System.out.println("6666666");
 	}
 
 }
