87a9d0ed5d90521229052ddb3137eb5dae4b7be7
hxf <hxf199248@163.com>
2016-05-27 21:33:17 +0800

    冲突

--- src/com/atguigu/crm/test/TestGit.java
+++ src/com/atguigu/crm/test/TestGit.java
@@ -4,6 +4,8 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("1111111");
 		System.out.println("2222222");
+		System.out.println("3333333");
+		System.out.println("4444444");
 		System.out.println("5555555");
 		System.out.println("6666666");
 	}
